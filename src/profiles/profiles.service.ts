5309df7ca842a4717e6505cfcdab69c12960bb28
Nizaam Yunus <nizaamcoleyunus18@gmail.com>
2026-07-11 15:38:33 +0200

    Added examples of error handling

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -51,7 +51,7 @@ export class ProfilesService {
         const matchingProfile = this.profiles.find((existingProfile) => existingProfile.id === id);
 
         if (!matchingProfile) {
-            return {};
+            throw new NotFoundException(`Profile with ID ${id} not found.`);
         }
 
         matchingProfile.name = updateProfileDto.name;
@@ -63,8 +63,10 @@ export class ProfilesService {
     remove(id: string): void {
         const matchingProfileIndex = this.profiles.findIndex((profile) => profile.id === id);
 
-        if (matchingProfileIndex > -1) {
-            this.profiles.splice(matchingProfileIndex, 1);
+        if (matchingProfileIndex === -1) {
+            throw new NotFoundException(`Profile with ID ${id} not found.`);
         }
+
+        this.profiles.splice(matchingProfileIndex, 1);
     }
 }
